6678d33d3baa4c8326bab975b431e2807ed14cbd
syzer3@gmail.com <syzer3@gmail.com>
2016-06-03 12:59:00 +0200

    tests with larger file

--- highland.js
+++ highland.js
@@ -3,47 +3,52 @@ const highland = require('highland')
 const fs = require('fs')
 
 function dataStream() {
-    return fs.createReadStream('./data.json')
+    return fs.createReadStream('./data1gb.json')
         .pipe(jsonStream.parse('*'))
 }
 
-set('iterations', 10)
+set('iterations', 1)
 set('concurrency', 2)
 
 suite('Test highland.js', () => {
     bench('Test map', next =>
         highland(dataStream())
-        .map(data => data.friends)
-        .on('data', () => {})
-        .on('end', next)
+            .map(data => data.posts)
+            .on('data', () => {
+            })
+            .on('end', next)
     )
 
     bench('Test reduce', next =>
         highland(dataStream())
-        .reduce(0, (acc, curr) => acc + curr)
-        .on('data', () => {})
-        .on('end', next)
+            .map(e => e.accountHistory.map(el => parseInt(el.amount)))
+            .reduce(0, (acc, curr) => acc + curr)
+            .on('data', () => {
+            })
+            .on('end', next)
     )
 
     bench('Test some', next =>
         highland(dataStream())
-        .map(data => data.age)
-        .find(age => age === 45)
-        .on('data', () => {})
-        .on('end', next)
+            .map(data => data.accountHistory)
+            .find(el => el.amount === '0.00')
+            .on('data', () => {
+            })
+            .on('end', next)
     )
 
-    bench('Test something more', next =>
-        highland(dataStream())
-        .map(data => ({
-            friends: data.friends,
-            tags: data.tags
-        }))
-        .filter(data => data.tags.length > 1)
-        .map(data => data.tags.concat(data.friends))
-        .map(data => data.length)
-        .reduce(0, (acc, curr) => acc + curr)
-        .on('data', () => {})
-        .on('end', next)
-    )
+    // bench('Test something more', next =>
+    //     highland(dataStream())
+    //         .map(data => ({
+    //             friends: data.friends,
+    //             tags: data.tags
+    //         }))
+    //         .filter(data => data.tags.length > 1)
+    //         .map(data => data.tags.concat(data.friends))
+    //         .map(data => data.length)
+    //         .reduce(0, (acc, curr) => acc + curr)
+    //         .on('data', () => {
+    //         })
+    //         .on('end', next)
+    // )
 })
--- ost.js
+++ ost.js
@@ -3,47 +3,52 @@ const ost = require('object-stream-tools')
 const fs = require('fs')
 
 function dataStream() {
-    return fs.createReadStream('./data.json')
+    return fs.createReadStream('./data1gb.json')
         .pipe(jsonStream.parse('*'))
 }
 
-set('iterations', 10)
+set('iterations', 1)
 set('concurrency', 2)
 
 suite('Test ost.js', () => {
     bench('Test map', next => {
         dataStream()
-            .pipe(ost.map(data => data.friends))
-            .on('data', () => {})
-            .on('end', next);
+            .pipe(ost.map(data => data.posts))
+            .on('data', () => {
+            })
+            .on('end', next)
     })
 
     bench('Test reduce', next =>
         dataStream()
-        .pipe(ost.reduce((acc, curr) => acc + curr, 0))
-        .on('data', () => {})
-        .on('end', next)
+            .pipe(ost.map(e => e.accountHistory.map(el => parseInt(el.amount))))
+            .pipe(ost.reduce((acc, curr) => acc + curr, 0))
+            .on('data', () => {
+            })
+            .on('end', next)
     )
 
     bench('Test some', next =>
         dataStream()
-        .pipe(ost.map(data => data.age))
-        .pipe(ost.some(age => age === 45))
-        .on('data', () => {})
-        .on('end', next)
+            .pipe(ost.map(data => data.accountHistory))
+            .pipe(ost.find(el => el.amount === '0.00'))
+            .on('data', () => {
+            })
+            .on('end', next)
     )
 
-    bench('Test something more', next =>
-        dataStream()
-        .pipe(ost.map(data => ({
-            friends: data.friends,
-            tags: data.tags
-        })))
-        .pipe(ost.filter(data => data.tags.length > 1))
-        .pipe(ost.map(data => data.tags.concat(data.friends)))
-        .pipe(ost.map(data => data.length))
-        .pipe(ost.reduce((acc, curr) => acc + curr, 0))
-        .on('data', () => {})
-        .on('end', next)
-    )
+    // bench('Test something more', next =>
+    //     dataStream()
+    //         .pipe(ost.map(data => ({
+    //             friends: data.friends,
+    //             tags: data.tags
+    //         })))
+    //         .pipe(ost.filter(data => data.tags.length > 1))
+    //         .pipe(ost.map(data => data.tags.concat(data.friends)))
+    //         .pipe(ost.map(data => data.length))
+    //         .pipe(ost.reduce((acc, curr) => acc + curr, 0))
+    //         .on('data', () => {
+    //         })
+    //         .on('end', next)
+    // )
 })
